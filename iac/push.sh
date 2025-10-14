f17664d381ad3b782209cb648658b28b73fd472d
jbenabra <jihadbenabra@ggmail.com>
2025-10-14 14:45:02 +0000

    fix run

--- iac/push.sh
+++ iac/push.sh
@@ -6,6 +6,9 @@ VERSION="${VERSION:-v50}"
 BUILD_OUTPUT_DIR="${BUILD_OUTPUT_DIR:-run}"
 BUILD_DIR="${ROOT_DIR}/${BUILD_OUTPUT_DIR}"
 
+echo $BUILD_OUTPUT_DIR
+echo $BUILD_DIR
+
 if [[ ! -d "${BUILD_DIR}" ]]; then
   echo "Build output directory '${BUILD_DIR}' does not exist. Run 'npm run build' to generate it." >&2
   exit 1
