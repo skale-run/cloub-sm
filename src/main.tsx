3f121c079c0b06c9d004fde04c4247ba50cf67fd
Jihad BENABRA <jistur@gmail.com>
2025-10-14 15:05:42 +0200

    Integrate theme tokens with light and dark support

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { applyThemeToDocument } from './theme.js'
+import './usethis.css'
 import './index.css'
 import App from './App.tsx'
+
+applyThemeToDocument()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
